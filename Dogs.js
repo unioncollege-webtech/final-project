62c9e30308c30d2ff73c3470615943452a121512
Samuel <sdtorres2@Gmail.com>
2015-12-10 10:12:49 -0600

    second update

---
+++ Dogs.js
@@ -0,0 +1,73 @@
+var dogs = [
+
+  {
+    breed: "Golden Retriver",
+    size: "Meduim",
+    energy: "High",
+    personality: "Friendly",
+    lifestyle: "Active",
+    grooming: "Moderate"
+    image: ""
+  },
+
+  {
+    breed: "Beagle",
+    size: "Small",
+    energy: "Meduim",
+    personality: "Friendly",
+    lifestyle: "Semi-active",
+    grooming: "Little"
+    image: ""
+  },
+
+  {
+    breed: "Shiba Inu",
+    size: "Small",
+    energy: "Meduim",
+    personality: "Moderately-Friendly",
+    lifestyle: "Semi-active",
+    grooming: "Moderate",
+    image: ""
+  },
+
+  {
+    breed: "German Shepherd",
+    size: "Large",
+    energy: "Meduim",
+    personality: "Moderately-Friendly",
+    lifestyle: "Active",
+    grooming: "Little",
+    image: ""
+  },
+
+  {
+    breed: "German Shepherd",
+    size: "Large",
+    energy: "Meduim",
+    personality: "Moderately-Friendly",
+    lifestyle: "Active",
+    grooming: "Little",
+    image: ""
+  },
+
+  {
+    breed: "Basenji",
+    size: "Small",
+    energy: "High",
+    personality: "Moderately-Friendly",
+    lifestyle: "Semi-active",
+    grooming: "Little",
+    image: ""
+  }
+
+  {
+    breed: "Bulldog",
+    size: "Small",
+    energy: "Low",
+    personality: "Friendly",
+    lifestyle: "Sedentary",
+    grooming: "Little",
+    image: ""
+  },
+
+]
