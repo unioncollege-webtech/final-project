10e7fce8d219e01efc1b836b987a927846046a00
sdtorres2 <sdtorres2@gmail.com>
2015-12-12 20:18:51 +0000

    updated server.js again

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
+var exphbs = require('express3-handlebars');
+
+app.engine('handlebars',
+    exphbs ({defaultLayout: 'main'}));
+    
+app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res){
-    res.send('Hello, World');
+    res.render('index');
 });
+
 app.use('final-project', express.static('final-project'));
 
 app.listen(process.env.PORT, process.env.IP);
